836f499c4ddbd93b904bb5c9d474a93110853180
NFC6664 <2954834129@qq.com>
2025-07-27 10:26:24 +0800

    进行了对依赖注入的添加

--- DailyPoetryM/MauiProgram.cs
+++ DailyPoetryM/MauiProgram.cs
@@ -14,7 +14,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-        builder.Services.AddSingleton<IPreferences>();
+
 
 #if DEBUG
         builder.Logging.AddDebug();
